9ad900e93fa7985ed8d4ebd3a571035e9415af5a
James Pike <jamesandrewpike@gmail.com>
2023-10-13 15:03:36 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
     width: '100%',
     zIndex: 1000, // You can adjust the z-index value if needed
     
-    
     // Add other styling as needed, like background color, box shadow, etc.
   };
 
@@ -26,7 +25,7 @@ function App() {
     <>
 
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-<div style={navbarStyle}>
+<div style={navbarStyle} className=''>
 <Navbar fluid rounded>
       <Navbar.Brand href="https://flowbite-react.com">
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 ml-3 mr-3 sm:h-9" alt="Flowbite React Logo" />
@@ -38,7 +37,7 @@ function App() {
       </div>
       <Navbar.Collapse>
       <ul className="flex flex-col p-4 -mt-1.5 font-medium text-left border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"> {/* Apply the classes here */}
-      <Navbar.Link href="#" active className="bg-blue-600"> {/* Apply the class to the selected item */}
+      <Navbar.Link href="#" active className="bg-blue-600 rounded"> {/* Apply the class to the selected item */}
                   Home
                 </Navbar.Link>
                 <Navbar.Link href="#">About</Navbar.Link>
